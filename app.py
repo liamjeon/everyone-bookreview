053844ffee851ee159dde815a58def98c5fd76b1
liamjeon <89363456+liamjeon@users.noreply.github.com>
2021-11-05 13:34:51 +0900

    mongodb local로 변경

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ app = Flask(__name__)
 
 from pymongo import MongoClient
 
+# client = MongoClient('mongodb://test:test@localhost', 27017)
 client = MongoClient('localhost', 27017)
-db = client.dbbook
+db = client.dbbookreview
 
 SECRET_KEY = '18'
 
